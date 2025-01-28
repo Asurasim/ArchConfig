477221726d738e1967ab34e1820b7f6581935ac1
Asurasim <unknownstestmail@gmail.com>
2025-01-28 18:56:42 +0000

    Add files and easy symlinking

---
+++ nvim/lua/config/keymaps.lua
@@ -0,0 +1,8 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+local map = LazyVim.safe_keymap_set
+
+map("n", "<leader>ii", "<cmd>e ~/Notes/Index.md<cr>", { desc = "Go to notes directory", remap = true })
+map("n", "<leader>ig", "<cmd>MarknavJump<cr>", { desc = "Enter markdown link", remap = true })
---
+++ symlink.sh
@@ -0,0 +1,4 @@
+ln -s ~/Config/nvim ~/.config/nvim
+ln -s ~/Config/mpv ~/.config/mpv
+ln -s ~/Config/OpenTabletDriver ~/.config/OpenTabletDriver
+ln -s ~/Config/yazi ~/.config/yazi
