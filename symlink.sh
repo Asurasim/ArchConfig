4b9620e72687574872a70df0be28327dc2476354
Asurasim <unknownstestmail@gmail.com>
2025-01-28 22:04:07 +0000

    Lots of changes
    Waybar + Hyprland + OTD

--- symlink.sh
+++ symlink.sh
@@ -7,5 +7,6 @@ ln -s ~/Config/waybar ~/.config/waybar
 ln -s ~/Config/ncmpcpp/ ~/.config/ncmpcpp
 ln -s ~/Config/mpd ~/.config/mpd
 ln -s ~/Config/feh ~/.config/feh
+ln -s ~/Config/uwsm ~/.config/uwsm
 
 ln -s ~/Config/wallpapers ~/Pictures/Wallpapers
