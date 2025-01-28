3fdc6416618c53b7de27adfbd06b1ab9ab386026
Asurasim <unknownstestmail@gmail.com>
2025-01-28 21:20:41 +0000

    Lots of fixes and ~ *correcting* ~

--- symlink.sh
+++ symlink.sh
@@ -2,3 +2,10 @@ ln -s ~/Config/nvim ~/.config/nvim
 ln -s ~/Config/mpv ~/.config/mpv
 ln -s ~/Config/OpenTabletDriver ~/.config/OpenTabletDriver
 ln -s ~/Config/yazi ~/.config/yazi
+ln -s ~/Config/hypr ~/.config/hypr
+ln -s ~/Config/waybar ~/.config/waybar
+ln -s ~/Config/ncmpcpp/ ~/.config/ncmpcpp
+ln -s ~/Config/mpd ~/.config/mpd
+ln -s ~/Config/feh ~/.config/feh
+
+ln -s ~/Config/wallpapers ~/Pictures/Wallpapers
